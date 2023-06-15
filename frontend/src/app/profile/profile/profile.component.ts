5083dce6d8e3ce28cf847e9852e42a49745be6cf
FausUCU <88690038+FausUCU@users.noreply.github.com>
2023-06-15 16:26:20 -0300

    TS and css for profile commponenet
    import and implement the profile service in the ts and its functions and made the css of profile component

--- frontend/src/app/profile/profile/profile.component.ts
+++ frontend/src/app/profile/profile/profile.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { UserService } from 'src/app/services/profile.service';
+import { UserInterface } from 'src/app/interfaces/IUser';
 
 @Component({
   selector: 'app-profile',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  currentUser: UserInterface;
 
+  constructor(private userService: UserService) {
+    this.currentUser = this.userService.getUser();
+  }
+
+  changeUsername(): void {
+    const newUsername = prompt('Enter a new username:');
+    if (newUsername) {
+      this.userService.changeUsername(newUsername);
+      this.currentUser = this.userService.getUser(); 
+    }
+  }
+
+  changePassword(): void {
+    const newPassword = prompt('Enter a new password:');
+    if (newPassword) {
+      this.userService.changePassword(newPassword);
+      this.currentUser = this.userService.getUser(); 
+    }
+  }
 }
