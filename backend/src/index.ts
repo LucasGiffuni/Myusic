416034d5cae3ced3c6265b91c923e01232c2d329
Sofihernandez610 <sofihernande123@gmail.com>
2023-06-15 22:50:41 -0300

    ho

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
 app.use('/user', userRoutes);
 app.use('/album', albumRoutes);
 
+
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
