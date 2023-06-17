17b4fd0588293bd59a131b6fcf098987ebf296bb
SantiagoVaraldo <48606980+SantiagoVaraldo@users.noreply.github.com>
2023-06-17 19:31:27 -0300

    Sections add album and add song

--- frontend/src/app/home/home-page/home-page.component.ts
+++ frontend/src/app/home/home-page/home-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlbumComponent } from './album/album.component';
+import { SongComponent } from './song/song.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, AlbumComponent],
+  imports: [CommonModule, AlbumComponent, SongComponent],
   template: `
     <div id="library-component">
-      <h1 id="library-component-album-title">Add album</h1>
+      <h1 class="library-component-title">Add album</h1>
       <div id="library-component-album-list">
         <app-album id="album-component"></app-album>
         <app-album id="album-component"></app-album>
@@ -18,6 +19,17 @@ import { AlbumComponent } from './album/album.component';
         <app-album id="album-component"></app-album>
         <app-album id="album-component"></app-album>
         <app-album id="album-component"></app-album>
+      </div>
+    </div>
+    <div class = "library-component">
+      <h1 class = "library-component-title">Add song</h1>
+      <div class = "library-component-song-list">
+        <app-song class = "song-component"></app-song>
+        <app-song class = "song-component"></app-song>
+        <app-song class = "song-component"></app-song>
+        <app-song class = "song-component"></app-song>
+
+
       </div>
     </div>
   `,
