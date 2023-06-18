9d192ba814505af77943d52ac11845fc1cdf2861
SantiagoVaraldo <48606980+SantiagoVaraldo@users.noreply.github.com>
2023-06-17 21:21:57 -0300

    Section latest songs

--- frontend/src/app/home/home-page/home-page.component.ts
+++ frontend/src/app/home/home-page/home-page.component.ts
@@ -8,17 +8,18 @@ import { SongComponent } from './song/song.component';
   standalone: true,
   imports: [CommonModule, AlbumComponent, SongComponent],
   template: `
-    <div id="library-component">
+    <div class="library-component">
       <h1 class="library-component-title">Add album</h1>
-      <div id="library-component-album-list">
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
+      <div class="library-component-album-list">
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
       </div>
     </div>
     <div class = "library-component">
@@ -28,8 +29,20 @@ import { SongComponent } from './song/song.component';
         <app-song class = "song-component"></app-song>
         <app-song class = "song-component"></app-song>
         <app-song class = "song-component"></app-song>
-
-
+      </div>
+    </div>
+    <div class = "library-component">
+      <h1 clas = "library-component-title">The latest</h1>
+      <div class = library-component-songLatest-list>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
       </div>
     </div>
   `,
