322b93bee861bf56d9dc2c45b8a904935d358edc
FausUCU <88690038+FausUCU@users.noreply.github.com>
2023-06-18 18:56:00 -0300

    route
    add route to profileComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { LibraryComponent } from './home/library/library.component';
+import { ProfileComponent } from './profile/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: LibraryComponent }
+  { path: 'home', component: LibraryComponent },
+  { path: 'profile', component: ProfileComponent }
 
 
 
