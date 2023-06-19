be9643d747253011eda30081f6bc46bd574d3af9
Lucas Giffuni <lucasgiffuni@gmail.com>
2023-06-18 23:37:04 -0300

    Routing corrections

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { HomeComponent } from './home/home.component';
-import { LibraryComponent } from './home/library/library.component';
 import { ProfileComponent } from './profile/profile/profile.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { SongDetailComponent } from './home/home-page/song-detail/song-detail.component';
@@ -11,7 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: LibraryComponent },
+  { path: 'home', component: HomePageComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'homePage', component: HomePageComponent },
   { path: 'songDetail', component: SongDetailComponent }
