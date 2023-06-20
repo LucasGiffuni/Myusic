ee5d0dc6799b48f5cb65ba9c5df4c60cc736a085
SantiagoVaraldo <48606980+SantiagoVaraldo@users.noreply.github.com>
2023-06-20 19:11:47 -0300

    fixing columns in section add album and adding css to main

--- frontend/src/app/home/home-page/home-page.component.ts
+++ frontend/src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlbumComponent } from './album/album.component';
 import { SongComponent } from './song/song.component';
@@ -8,17 +8,21 @@ import { SongComponent } from './song/song.component';
   standalone: true,
   imports: [CommonModule, AlbumComponent, SongComponent],
   template: `
-    <div id="library-component">
+    <div class="library-component">
       <h1 class="library-component-title">Add album</h1>
-      <div id="library-component-album-list">
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
-        <app-album id="album-component"></app-album>
+      <div #listAlbums class="library-component-album-list">
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
+        <app-album class="album-component"></app-album>
       </div>
     </div>
     <div class = "library-component">
@@ -28,11 +32,38 @@ import { SongComponent } from './song/song.component';
         <app-song class = "song-component"></app-song>
         <app-song class = "song-component"></app-song>
         <app-song class = "song-component"></app-song>
-
-
+      </div>
+    </div>
+    <div class = "library-component">
+      <h1 class = "library-component-title">The latest</h1>
+      <div class = library-component-songLatest-list>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
+        <app-song class = "songLatest-component"></app-song>
       </div>
     </div>
   `,
   styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent {}
+export class HomePageComponent implements AfterViewInit {
+  @ViewChild('listAlbums', {static:false}) listAlbums! : ElementRef;
+  
+  ngAfterViewInit(){
+    const albumList = this.listAlbums.nativeElement;
+    const albums = albumList.querySelectorAll('.album-component');
+    const totalAlbums = albums.length;
+
+    const containerAlbum = document.querySelector('.library-component-album-list');
+    if (containerAlbum != null){
+      const numColumns = Math.ceil(totalAlbums / 2);
+      //@ts-ignore
+      containerAlbum.style.gridTemplateColumns = `repeat(${numColumns}, 50%)`;
+    }
+  }
+}
---
+++ frontend/src/app/services/services.service.js
@@ -0,0 +1,12 @@
+import { createConnection } from 'mysql';
+
+var con = createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
