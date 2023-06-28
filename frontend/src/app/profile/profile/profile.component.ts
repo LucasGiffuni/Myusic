0e6751c58ad8f051993dd512865536b917a2984f
Lucas Giffuni <55067106+LucasGiffuni@users.noreply.github.com>
2023-06-28 17:57:42 -0300

    fead logout: logout with cookie remove

--- frontend/src/app/profile/profile/profile.component.ts
+++ frontend/src/app/profile/profile/profile.component.ts
@@ -1,50 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserService } from 'src/app/services/profile.service';
 import { UserInterface } from 'src/app/interfaces/IUser';
+import { MatIconModule } from '@angular/material/icon';
 
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'src/app/services/cookie.service';
 
 @Component({
   selector: 'app-profile',
-  
+  standalone: true,
+  imports: [MatIconModule],
   template: `
-  
-  <div class="container">
-  
+    <div class="container">
+      <div class="profile">
+        <div class="form-group">
+          <h1>User Profile:</h1>
+          <label for="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value="{{ username }}"
+            placeholder="Username"
+            disabled
+          />
+          <button (click)="clickButton('/changeUsserName')">Change</button>
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value="{{ password }}"
+            placeholder="Password"
+            disabled
+          />
+          <button (click)="clickButton('/changePassword')">Change</button>
+        </div>
 
-  <div class="profile">
-    <div class="form-group">
-      <h1>User Profile:</h1>
-      <label for="username">Username:</label>
-      <input type="text" id="username" value="{{username}}" placeholder="Username" disabled>
-      <button (click)="clickButton('/changeUsserName')">Change</button>
-      
+        <button
+          mat-flat-button
+          color="primary"
+          mat-dialog-close
+          cdkFocusInitial
+          (click)="logout()"
+        >
+          Logout
+        </button>
+      </div>
     </div>
-    <div class="form-group">
-      <label for="password">Password:</label>
-      <input type="password" id="password" value="{{password}}" placeholder="Password" disabled>
-      <button (click)="clickButton('/changePassword')">Change</button>
-      
-      
-    </div>
-  </div>
-</div>
-  
- `,
-  styleUrls: ['./profile.component.css']
+  `,
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
   currentUser: UserInterface;
-  username: string = "";
-  password: string = "";
+  username: string = '';
+  password: string = '';
+  cookieService: CookieService = inject(CookieService);
+
 
   onFocusOutUsername(event: any) {
-    this.username = event.target.value
+    this.username = event.target.value;
   }
   onFocusOutPassword(event: any) {
-    this.password = event.target.value
+    this.password = event.target.value;
   }
-  
+
   constructor(private userService: UserService, private router: Router) {
     this.currentUser = this.userService.getUser();
   }
@@ -53,12 +73,17 @@ export class ProfileComponent {
     const newUsername = prompt('Enter a new username:');
     if (newUsername) {
       this.userService.changeUsername(newUsername);
-      this.currentUser = this.userService.getUser(); 
+      this.currentUser = this.userService.getUser();
     }
   }
   clickButton(path: string) {
     this.router.navigate([path]);
   }
 
-
+  logout() {
+    this.router.navigate(["login"]);
+    this.cookieService.remove("SESSIONID")
+    this.cookieService.remove("USERID")
+    this.cookieService.remove("SELECTEDSONG")
+  }
 }
