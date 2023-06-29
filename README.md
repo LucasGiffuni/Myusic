e728a296e730bd3d20271201cd78de9df5a3af5a
Lucas Giffuni <lucasgiffuni@gmail.com>
2023-06-29 15:23:49 -0300

    feat back to login when 403 status code

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-set NODE_ENV=development&&npm run dev
+# MYUSIC #
 
-NODE_ENV=development npm run dev
 
+### Comandos para iniciar el proyecto ###
+
+#### Ejecutar backend ####
+
+- Si el S.O es WINDOWS:
+    npm i
+    set NODE_ENV=development&&npm run dev
+
+- Si el S.O es MACOS:
+    npm i
+    NODE_ENV=development npm run dev
+
+#### Ejecutar Frontend ####
+    npm i   
+    npm run start
--- frontend/src/app/home/home-page/create-song-dialog/create-song-dialog.component.ts
+++ frontend/src/app/home/home-page/create-song-dialog/create-song-dialog.component.ts
@@ -9,19 +9,40 @@ import { IAlbum } from 'src/app/interfaces/IAlbum';
 import { MAT_SNACK_BAR_DATA, MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { IResponse } from 'src/app/interfaces/IResponse';
 import { ISong } from 'src/app/interfaces/ISong';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-song-dialog',
   standalone: true,
   imports: [CommonModule, MatButtonModule, MatDialogModule, MatSnackBarModule],
   template: `
-  <h1 mat-dialog-title>CreateAlbumDialog</h1>
+  <h1 mat-dialog-title>Create Song</h1>
   <div mat-dialog-content>
-  <input type="text" id="songTitle" placeholder="Song Title"  (input)="SongTitleInput($event)">
+    <div>
+    <input type="text" id="songTitle" placeholder="Song Title"  (input)="SongTitleInput($event)">
+
+    </div>
+
   <input type="text" id="songGenre" placeholder="Song Genre" (input)="SongGenreInput($event)">
   <input type="text" id="releaseDate" placeholder="Release Date (YYYY-MM-DD)"  (input)="SongDateInput($event)">
+  <div>
   <input type="text" id="referenceLink" placeholder="Reference Link" (input)="SongLinkInput($event)">
+  <i
+        id="showReferenceLink"
+        class="material-icons w3-xlarge"
+        (click)="showLinkInfo()"
+        >info</i
+      >
+  </div>
   <input type="text" id="autor" placeholder="Song Author / Band"  (input)="SongAutorInput($event)">
+  <div>
   <input type="text" id="image" placeholder="Song image" (input)="SongimageInput($event)">
+  <i
+        id="showImage"
+        class="material-icons w3-xlarge"
+        (click)="showImageInfo()"
+        >info</i
+      >
+  </div>
   </div>
   <div mat-dialog-actions>
     <button mat-button mat-dialog-close>Cancel</button>
@@ -31,7 +52,7 @@ import { ISong } from 'src/app/interfaces/ISong';
   styleUrls: ['./create-song-dialog.component.css']
 })
 export class CreateSongDialogComponent {
-  constructor(public dialogRef: MatDialogRef<CreateSongDialogComponent>, private _snackBar: MatSnackBar) { }
+  constructor(public dialogRef: MatDialogRef<CreateSongDialogComponent>, private _snackBar: MatSnackBar, private router: Router) { }
   albumService: AlbumService = inject(AlbumService);
   songService: SongService = inject(SongService);
   cookieService: CookieService = inject(CookieService);
@@ -39,9 +60,9 @@ export class CreateSongDialogComponent {
   songTitle: string = "";
   genero: string = "";
   fechaLanzamiento: string = "";
-  linkReferencia: string = "";
+  linkReferencia: string = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
   autor: string = "";
-  imagen: string = "";
+  imagen: string = "https://img.youtube.com/vi/dQw4w9WgXcQ/sddefault.jpg";
 
 
 
@@ -65,6 +86,16 @@ export class CreateSongDialogComponent {
     this.imagen = event.target.value
   }
 
+
+  showLinkInfo() {
+    this.openInfoSnackBar("Para una mejor experiencia el link debe de ser de youtube, con el formato \n  https://www.youtube.com/watch?v=dQw4w9WgXcQ \n dejar vacio para default", "Cerrar")
+
+  }
+  showImageInfo() {
+    this.openInfoSnackBar("(WIP) - provisionalmente las imagenes se extraen de internet \n https://img.youtube.com/vi/dQw4w9WgXcQ/sddefault.jpg \n dejar vacio para default", "Cerrar")
+
+  }
+
   createAlbum() {
 
     const data: ISong = {
@@ -79,19 +110,27 @@ export class CreateSongDialogComponent {
       idUsuario: 24
     }
 
-    if (this.songTitle || this.genero || this.fechaLanzamiento || this.autor || this.imagen) {
-      if(this.songTitle.length <= 30 || this.autor.length <= 15){
+    if (this.songTitle || this.genero || this.fechaLanzamiento || this.autor) {
+      if (this.songTitle.length <= 30 || this.autor.length <= 15) {
+
+
         this.songService.createSong(data.idUsuario, data).then((response: IResponse<any>) => {
-        if (response.Result.statuscode === "200") {
-          this.openSnackBar("Song " + this.songTitle + " creado correctamente!", "Cerrar")
-          setTimeout(() => {
-            location.reload();
-          }, 30000); 
-        } else {
-          this.openSnackBar("ERROR: " + response.Result.statustext, "Cerrar")
-        }
-      });
-      }else{
+          if (response.Result.statuscode === "403") {
+            this.openSnackBar("Session expired", "Cerrar")
+
+            this.router.navigate(['/login']);
+          } else {
+            if (response.Result.statuscode === "200") {
+              this.openSnackBar("Song " + this.songTitle + " creado correctamente!", "Cerrar")
+              setTimeout(() => {
+                location.reload();
+              }, 30000);
+            } else {
+              this.openSnackBar("ERROR: " + response.Result.statustext, "Cerrar")
+            }
+          }
+        });
+      } else {
         this.openSnackBar("Titulo debe ser menor o igual a 30 caracteres y autor menor o igual a 15 caracteres", "Cerrar")
       }
     } else {
@@ -104,5 +143,8 @@ export class CreateSongDialogComponent {
     this._snackBar.open(message, action, {
       duration: 2500
     });
+  }
+  openInfoSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
   }
 }
